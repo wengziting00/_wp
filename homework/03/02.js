241a417e0cf7c208b01493ca9735a67ae93f8763
wengziting00 <s111310519@student.nqu.edu.tw>
2025-05-03 15:00:34 +0800

    Update 02.js

---
+++ homework/03/02.js
@@ -0,0 +1,10 @@
+/*運算與條件判斷
+宣告兩個變數 a = 10 和 b = 20，比較它們的大小。
+如果 a 大於 b，輸出 "a 大於 b"，否則輸出 "a 小於等於 b"。*/
+let a = 10;
+let b = 20;
+if (a> b){
+  console.log("a > b");
+} else{
+  console.log("a <=b");
+}
