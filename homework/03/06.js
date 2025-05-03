853da49f0cfe5c9c6e0ce74104eb03031da310b8
wengziting00 <s111310519@student.nqu.edu.tw>
2025-05-03 15:01:14 +0800

    Update 06.js

---
+++ homework/03/06.js
@@ -0,0 +1,9 @@
+/*遞迴函數
+定義一個遞迴函數 factorial(n)，計算 n 的階乘（n!）。
+例如 factorial(5) 應該回傳 120。*/
+function factorial(n) {
+    if (n === 0 || n === 1){
+      return 1;
+    }
+    return n * factorial(n - 1);
+  }
