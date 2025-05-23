8d842cccea0fa19dbb7ec637403384ad4f1a803e
wengziting00 <s111310519@student.nqu.edu.tw>
2025-05-23 15:33:00 +0800

    Create 05.py

---
+++ homework/08/05.py
@@ -0,0 +1,14 @@
+def grade(score):
+    if not (0 <= score <= 100):
+        raise ValueError("0~100")
+
+    if score >= 90:
+        return 'A'
+    elif score >= 80:
+        return 'B'
+    elif score >= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F'
